4588cba9501c3de0140e4b55bbb17991c015249c
Ashish Garg <ashishgarg2709@gmail.com>
2022-02-17 22:27:37 +0530

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Trading />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Trading from './components/Trading.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Trading
   }
 }
 </script>
---
+++ src/components/Trading.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Ashish Garg</div>
+</template>
+
+<script>
+import "buefy/dist/buefy.css";
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
