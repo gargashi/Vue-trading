e9ec9b5ce78d050f872de2d7cd7f871813210b2c
Ashish Garg <ashishgarg2709@gmail.com>
2022-02-18 01:10:51 +0530

    API call and data load

--- src/components/Trading.vue
+++ src/components/Trading.vue
@@ -1,12 +1,49 @@
 <template>
   <div>Ashish Garg</div>
+  <!-- <div v-for="price in priceDetail" :key="price">{{ price["1. open"] }}</div> -->
+  <div>{{ tradingYValues }}</div>
 </template>
 
 <script>
 import "buefy/dist/buefy.css";
+import axios from "axios";
+
 export default {
   data() {
-    return {};
+    return {
+      priceDetail: {},
+      tradingXValues: [],
+      tradingYValues: [],
+    };
+  },
+  mounted() {
+    this.setup()
+      .then((resp) => {
+        this.priceDetail = resp.data["Time Series (5min)"];
+        console.log(this.priceDetail);
+        for (var key in resp.data["Time Series (5min)"]) {
+          this.tradingXValues.push(key);
+          this.tradingYValues.push(
+            resp.data["Time Series (5min)"][key]["1. open"]
+          );
+        }
+      })
+      .catch((error) => {
+        return error;
+      });
+  },
+  methods: {
+    async setup() {
+      const baseURI =
+        "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=H2B6TNTJQPJUYI8C";
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      const resp = axios.get(baseURI, {
+        headers,
+      });
+      return resp;
+    },
   },
 };
 </script>
